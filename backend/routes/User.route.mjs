ef590ea3ccd08d0ed897f583891015e64b089497
muqeet007 <muqeetnabila308@gmail.com>
2025-07-12 07:44:55 +0500

    Added resgister and login functionality using jwt and bcrypt

--- backend/routes/User.route.mjs
+++ backend/routes/User.route.mjs
@@ -1,5 +1,8 @@
 import express from 'express'
-
+import User from '../models/User.model.mjs'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { SECRET_KEY } from '../config/config.mjs'
 const router=express.Router()
 
 router.get('/me',(req,res)=>
@@ -8,15 +11,90 @@ router.get('/me',(req,res)=>
     
 })
 
-router.post('/register',(req,res)=>
+router.post('/register',async(req,res)=>
 {
+    //Grabbing the details entered by the user.
+
+    const {name,email,password}=req.body
+
+    try
+    {
+        // Trying to see whether the user with the same email already exists , as email is unique
+    const user=await User.findOne({email})
     
-    
+    if(user)
+    {
+        return res.status(400).json({
+            message:"User already exists."
+        })
+    }
+
+    else
+    {
+        //Hashing the password to ensure security.
+
+        const hashedPassword=await bcrypt.hash(password,10)
+        const newUser=new User({name,email,password:hashedPassword})
+        await newUser.save()
+        return res.status(201).json({
+            message:"User has been created successfully."
+        })
+    }
+    }
+
+    catch(error)
+    {
+        return res.status(500).json({
+            message:`Error:${error.message}`
+        })
+    }
 })
 
-router.post('/login',(req,res)=>
+router.post('/login',async (req,res)=>
 {
-    
+    //Grabbing the credentials entered by the user
+    const {email,password}=req.body
+
+    try
+    {
+        const user=await User.findOne({email})
+
+        if(user)
+        {
+            // Checking the password
+
+            const isMatch=await bcrypt.compare(password,user.password)
+
+            if (!isMatch) return res.status(400).json({ message: "Invalid credentials." })
+            
+            else
+            {
+                const token=jwt.sign(
+                {
+                    userid:user._id,
+                    useremail:user.email
+                },
+                SECRET_KEY,
+                {expiresIn:'2h'}
+            )
+
+            return res.json({token})
+            }
+        }
+
+        else
+        {
+            return res.status(400).json(
+                {message:"Invalid Credentials"}
+            )
+        }
+    }
+    catch(error)
+    {
+         return res.status(500).json({
+            message:`Error:${error.message}`
+        })
+    }
     
 })
 
